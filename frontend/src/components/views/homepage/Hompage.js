ca38949d439ed44367d385dfbc5916bd04114169
Barbara <75448241+BarbaraPakula@users.noreply.github.com>
2021-08-09 14:54:47 +0200

    Add Search product to Homepage

--- frontend/src/components/views/homepage/Hompage.js
+++ frontend/src/components/views/homepage/Hompage.js
@@ -1,23 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Homepage.module.scss";
 import ProductCard from "../../common/productCard/ProductCard";
-import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+
+import TextField from "@material-ui/core/TextField";
+import Grid from "@material-ui/core/Grid";
+import SearchIcon from "@material-ui/icons/Search";
 
 import { getProducts as listProducts } from "../../../redux/productReducer";
 const Homepage = () => {
   const dispatch = useDispatch();
-
   const getProducts = useSelector((state) => state.getProducts);
   const { products, loading, error } = getProducts;
+  const [search, setSearch] = useState("");
+  const [filterProducts, setFilterProducts] = useState([]);
 
   useEffect(() => {
     dispatch(listProducts());
   }, [dispatch]);
 
+  useEffect(() => {
+    setFilterProducts(
+      products.filter((product) =>
+        product.name.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+      )
+    );
+  }, [products, search]);
+
   return (
-    <div className="homescreen">
-      <h2 className="homescreen__title">Latest Products</h2>
+    <div className={styles.homepage}>
+      <div className={styles.search}>
+        <Grid
+          container
+          spacing={1}
+          alignItems="flex-end"
+          onChange={(e) => setSearch(e.target.value)}
+          value={search}
+        >
+          <Grid item>
+            <SearchIcon />
+          </Grid>
+          <Grid item>
+            <TextField id="input-with-icon-grid" label="Search Products" />
+          </Grid>
+        </Grid>
+      </div>
 
       <div className={styles.card}>
         {loading ? (
@@ -25,7 +52,7 @@ const Homepage = () => {
         ) : error ? (
           <h2>{error}</h2>
         ) : (
-          products.map((product) => (
+          filterProducts.map((product) => (
             <div key={product._id} className={styles.card__item}>
               <ProductCard
                 name={product.name}
