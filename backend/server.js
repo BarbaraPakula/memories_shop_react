9c1a6b630e5203d84cb31555303b8ce88eab5049
Barbara <75448241+BarbaraPakula@users.noreply.github.com>
2021-09-14 17:59:21 +0200

    server

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,10 @@ app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'));
+});
+
 app.get('/', (req, res) => {
   res.json({ message: 'API running...' });
 });
